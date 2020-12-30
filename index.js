5fb4fc54e8bb064c723d714d5303869060599636
jeichlcof <74484217+jeichlcof@users.noreply.github.com>
2020-12-31 00:12:26 +0900

    initial-commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`Listening on : http://localhost:${PORT}`);
+};
+function handleHome(req, res) {
+  res.send("Hello from home");
+}
+
+app.get("/", handleHome);
+
+app.listen(4000, handleListening);
